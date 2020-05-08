85f1baa0a1332e4ad1318fc7e015785286ebf572
seongjun22 <64995973+seongjun22@users.noreply.github.com>
2020-05-08 10:06:16 +0900

    Add files via upload

---
+++ ch4/04-01rideif.py
@@ -0,0 +1,9 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> height = 152 # 탑승을 체크할 키를 대입
+>>> if 140 <= height:
+	print('롤러코스터 T-Express, 즐기세요!')
+
+	
+롤러코스터 T-Express, 즐기세요!
+>>> 
